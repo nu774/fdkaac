24c15bc275e4be500fdb7887b6114c1e322cc19e
nkh0472 <nkh0472@hotmail.com>
2021-06-17 10:09:21 +0800

    Add \0 at the end of VERSIONINFO strings
    When defining the version information string, all strings must be NULL-terminated strings, so add \0 at the end of the string.

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H
 #define VERSION_H
-#define VER_VERSION_STR "1.0.2"
+#define VER_VERSION_STR "1.0.2\0"
 #define VER_VERSION 1,0,2,0
 //The version parameter is two 32-bit integers, defined by four 16-bit integers, used only for Windows VERSIONINFO.
-#define VER_LEGALCOPYRIGHT_STR "(c) nu774 2013-2021"
+#define VER_LEGALCOPYRIGHT_STR "(c) nu774 2013-2021\0"
 #endif
