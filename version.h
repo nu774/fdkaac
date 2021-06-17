8dcbf2d561eeae8f25255b62cca9e04c4044c7ad
nkh0472 <nkh0472@hotmail.com>
2021-06-17 09:48:11 +0800

    Merge var VER_VERSION_STR and fdkaac_version

--- version.h
+++ version.h
@@ -1,7 +1,5 @@
 #ifndef VERSION_H
 #define VERSION_H
-const char *fdkaac_version = "1.0.2";
-
 #define VER_VERSION_STR "1.0.2"
 #define VER_VERSION 1,0,2,0
 //The version parameter is two 32-bit integers, defined by four 16-bit integers, used only for Windows VERSIONINFO.
